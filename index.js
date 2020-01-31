f75e74620347379c4f86670fc33c6b08e0323390
SimonCK666 <SimonYang223@163.com>
2020-01-31 16:30:08 +0800

    added a comment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // 在HTML渲染完成之后执行
 window.onload=function() {
     var excluder = this.document.getElementById("excluder");
+    // The user moves the mouse over an HTML element
     this.document.body.onmousemove = function(ev) {
         var x = ev.layerX - 105;
         var y = ev.layerY - 105;
