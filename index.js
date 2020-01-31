0dc3884b2112432cb272162079616cfbcbcce165
SimonCK666 <SimonYang223@163.com>
2020-01-31 13:06:23 +0800

    'perspective_web_making'

---
+++ index.js
@@ -0,0 +1,10 @@
+// 在HTML渲染完成之后执行
+window.onload=function() {
+    var excluder = this.document.getElementById("excluder");
+    this.document.body.onmousemove = function(ev) {
+        var x = ev.layerX - 105;
+        var y = ev.layerY - 105;
+
+        excluder.style.cssText = "transform:translate(" + x + "px," + y +"px); background-position:"+((x > 0) ? "-" : "") + Math.abs(x) +"px   "+((y > 0) ? "-" : "") + Math.abs(y) +"px;"
+    }
+}
